5e6d1fdd1104163cc8b77ce83daf9e904aa45103
lukasmasuch <lukas.masuch@gmail.com>
2022-06-14 01:52:04 +0200

    Update nullable state demo

--- prototypes/nullable-widgets/app.py
+++ prototypes/nullable-widgets/app.py
@@ -1,12 +1,50 @@
 import streamlit as st
 
 st.header("Nullable State Demo")
+
+st.subheader("Normal Date")
 st.code(
     """
-my_date = st.date_input("foo", None, clearable=True)
+my_date = st.date_input("Normal Date")
 st.write(my_date)
 """
 )
 
-my_date = st.date_input("Nullable Date Chooser", None, clearable=True)
+my_date = st.date_input("Normal Date")
+st.write("Value:", my_date)
+
+
+st.subheader("Normal Date & Clearable")
+st.code(
+    """
+my_date = st.date_input("Normal Date & Clearable", clearable=True)
+st.write(my_date)
+"""
+)
+
+my_date = st.date_input("Normal Date & Clearable", clearable=True)
+st.write("Value:", my_date)
+
+
+st.subheader("None Date & Clearable")
+st.code(
+    """
+my_date = st.date_input("None Date & Clearable", None, clearable=True)
+st.write(my_date)
+"""
+)
+
+my_date = st.date_input("None Date & Clearable", None, clearable=True)
+st.write("Value:", my_date)
+
+
+st.subheader("None Date & Not Clearable")
+st.code(
+    """
+my_date = st.date_input("None Date & Not Clearable", None, clearable=False)
+st.write(my_date)
+"""
+)
+
+my_date = st.date_input("None Date & Not Clearable", value=None, clearable=False)
 st.write("Value:", my_date)
