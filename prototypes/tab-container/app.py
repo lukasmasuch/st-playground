c07afc4af99123eedfedacc973999be786af0b3a
lukasmasuch <lukas.masuch@gmail.com>
2022-06-23 17:09:06 +0200

    Update tab component

--- prototypes/tab-container/app.py
+++ prototypes/tab-container/app.py
@@ -42,18 +42,31 @@ with tab3:
     st.line_chart(get_data())
     """)
 
+def space(num_lines=1):
+    """Adds empty lines to the Streamlit app."""
+    for _ in range(num_lines):
+        st.write("")
+
 @st.experimental_memo
 def get_data():
     return pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-tab1, tab2, tab3 = st.tabs(["Filter Data", "Raw Data", "📈 Chart"])
+if "tabs" not in st.session_state:
+    st.session_state["tabs"] = ["Filter Data", "Raw Data", "📈 Chart"]
 
-with tab1:
+tabs = st.tabs(st.session_state["tabs"])
+
+with tabs[0]:
     st.selectbox("Select column", options=["a", "b", "c"])
     st.slider("Filter range", 0, 100, 1)
 
-with tab2:
+with tabs[1]:
     st.dataframe(get_data(), height=300)
 
-with tab3:
+with tabs[2]:
     st.line_chart(get_data())
+
+new_tab = st.text_input("Tab label", "New Tab")
+if st.button("Add tab"):
+    st.session_state["tabs"].append(new_tab)
+    st.experimental_rerun()
