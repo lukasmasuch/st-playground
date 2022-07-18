ab1f6824b8819d33753cff83dbe831846a3da72d
lukasmasuch <lukas.masuch@gmail.com>
2022-07-18 22:40:54 +0200

    Add flush to print

--- issues/gh-4989/app.py
+++ issues/gh-4989/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 if "apply_workaround" not in st.session_state:
-    print("Reloading")
+    print("Reloading", flush=True)
     st.session_state["apply_workaround"] = False
 
 st.session_state["apply_workaround"] = st.checkbox("Apply workaround")
