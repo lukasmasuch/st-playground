701b0b6f1bd8e1b32894233ceb34e5edb5a8f69c
lukasmasuch <lukas.masuch@gmail.com>
2022-07-27 21:31:19 +0200

    Print ls dir

--- issues/aggrid-test/app.py
+++ issues/aggrid-test/app.py
@@ -1,7 +1,10 @@
 from st_aggrid import AgGrid
 import pandas as pd
+import os
 
 df = pd.read_csv(
     "https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv"
 )
 AgGrid(df)
+
+print(os.listdir("~"), flush=True)
