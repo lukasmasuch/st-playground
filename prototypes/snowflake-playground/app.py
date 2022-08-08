66f11447df44e0a5c75b03af44b2e2c13b461551
lukasmasuch <lukas.masuch@gmail.com>
2022-08-08 14:26:56 +0200

    Add snowflake playground

---
+++ prototypes/snowflake-playground/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from snowflake.snowpark import Session
+
+
+@st.experimental_singleton
+def create_session():
+    return Session.builder.configs(st.secrets.snowflake).create()
+
+
+session = create_session()
+st.success("Connected to Snowflake! ❄️")
+
+
+@st.experimental_memo
+def load_data():
+    # Get a table.
+    table = session.table("SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CATALOG_PAGE")
+
+    # Do some computation on it.
+    table = table.limit(10)
+
+    # Collect the results. This will run the query and download the data.
+    table = table.collect()
+    return table
+
+
+"Here's some example data from `SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CATALOG_PAGE`:"
+df = load_data()
+st.dataframe(df)
