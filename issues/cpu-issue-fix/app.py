0140bf4ae8980d02d23f2ce3c98b1aae9965829e
lukasmasuch <lukas.masuch@gmail.com>
2024-03-12 16:21:22 -0500

    Update apps

--- issues/cpu-issue-fix/app.py
+++ issues/cpu-issue-fix/app.py
@@ -7,10 +7,9 @@ import streamlit as st
 @st.cache_data
 def cache_something(num):
     time.sleep(2)
-    return 42
+    return num
 
 
-if st.button("Run"):
-    for _ in range(1000):
-        time.sleep(1)
-        cache_something(random.randint(0, 1000))
+time.sleep(1)
+cache_something(random.randint(0, 10000))
+st.rerun()
