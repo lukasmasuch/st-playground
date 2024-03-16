a9a3d70a7ba11997da58af7f9c3b97b2dcbe4953
lukasmasuch <lukas.masuch@gmail.com>
2024-03-15 20:14:35 -0500

    Update

--- issues/dialog-test-form/app.py
+++ issues/dialog-test-form/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 if st.toggle("Show data"):
-    num_rows = st.number_input("Num rows", 0, 500000, 25000, 1000)
+    num_rows = st.number_input("Num rows", 0, 500000, 100000, 1000)
     # create a random dataframe
     df = pd.DataFrame(
         np.random.randn(num_rows, 20), columns=("col %d" % i for i in range(20))
