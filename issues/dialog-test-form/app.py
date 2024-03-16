800612ce9a73968e25f34c66d4f1d0ac38f67226
lukasmasuch <lukas.masuch@gmail.com>
2024-03-15 19:36:38 -0500

    Add dialog tests

---
+++ issues/dialog-test-form/app.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+if st.toggle("Show data"):
+    num_rows = st.number_input("Num rows", 0, 500000, 25000, 1000)
+    # create a random dataframe
+    df = pd.DataFrame(
+        np.random.randn(num_rows, 20), columns=("col %d" % i for i in range(20))
+    )
+    st.dataframe(df)
+
+with st.form("my_form"):
+    st.write("Hello again!")
+    form_input = st.text_input("Enter something!")
+    st.form_submit_button("Submit")
+
+st.write(form_input)
---
+++ issues/dialog-test/app.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+if st.toggle("Show data"):
+    num_rows = st.number_input("Num rows", 0, 500000, 25000, 1000)
+    # create a random dataframe
+    df = pd.DataFrame(
+        np.random.randn(num_rows, 20), columns=("col %d" % i for i in range(20))
+    )
+    st.dataframe(df)
+
+
+@st.experimental_dialog("Simple Dialog")
+def simple_dialog():
+    st.write("Hello again!")
+    st.text_input("Enter something!", key="dialog2-input")
+
+    if st.button("Submit"):
+        st.rerun()
+
+
+if st.button("Open Dialog"):
+    simple_dialog()
+
+st.write(st.session_state.get("dialog2-input"))
